da21fb28d59e492cfeb669398836863296e14d13
redhuan <redhuan123@gmail.com>
2022-07-16 16:27:20 +0800

    added input and button action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,21 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  member: string[] = [];
+  newMember = '';
+  out() {
+    console.log('here');
+  }
+  addMember() {
+    this.member.push(this.newMember);
+    console.log(this.member);
+  }
+  onInput(member:string){
+    this.newMember = member;
+  }
 }
 //this test
